9275ec00ca1433edcb4293093fea8e4bd1332957
chaninkou <michaelkoulove@yahoo.com>
2019-10-14 23:39:21 -0700

    Great problem, Using dp bottomup approach to check if word inside string

--- src/wordBreak/CheckDictContainStringFunction.java
+++ src/wordBreak/CheckDictContainStringFunction.java
@@ -4,23 +4,20 @@ import java.util.List;
 
 public class CheckDictContainStringFunction {
     public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] f = new boolean[s.length() + 1];
-        
-        // Set<String> set = new HashSet<>();
-        // set.addAll(wordDict);
-        
-        f[0] = true;
-        
-        for(int i=1; i <= s.length(); i++){
-            for(int j = i - 1; j >= 0; j--){
-                System.out.println(i + s.substring(j, i));
-                if(f[j] && wordDict.contains(s.substring(j, i))){
-                    f[i] = true;
-                    break;
-                }
-            }
-        }
-        
-        return f[s.length()];
+    	boolean[] dp = new boolean[s.length() + 1];
+    	
+    	dp[0] = true;
+    	
+    	for(int i = 1; i <= s.length(); i++){
+    		for(int j = i - 1; j >= 0; j--){
+    			if(dp[j] && wordDict.contains(s.substring(j, i)) ){
+    				dp[i] = true;
+    				
+    				break;
+    			}
+    		}
+    	}
+    	
+    	return dp[s.length()];
     }
 }
--- src/wordBreak/Main.java
+++ src/wordBreak/Main.java
@@ -1,7 +1,24 @@
 package wordBreak;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
+		String s = "leetcode";
+		
+		List<String> wordDict = new ArrayList<>();
+		
+		wordDict.add("leet");
+		wordDict.add("code");
+		
+		System.out.println("String: " + s);
+		
+		System.out.println("Dictionary: " + wordDict);
+		
+		CheckDictContainStringFunction solution = new CheckDictContainStringFunction();
+		
+		System.out.println("Solution: " + solution.wordBreak(s, wordDict));
 		
 	}
 }
